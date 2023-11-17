c86dc4bf5ba1149885401dc2c07d9df98da73f1c
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2023-11-17 11:16:15 +0700

    (WIP) add augmentation_upto and example

--- Code/preprocess/export.py
+++ Code/preprocess/export.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import train_test_split
 from .preprocess import fetch_lfw_people
 import tqdm
 import numpy as np
@@ -10,17 +11,21 @@ from PIL import Image
 def export_dataset_objects(
     min_faces: int | None = 20,
     max_faces: int | None = None,
+    hard_limit: bool = False,
     shuffle=True,
     random_state=None,
+    test_size=0.2,
     verbose=True,
     augment=True,
     desired_shape: tuple[int, int] = (250, 250),
     augmentation_count: int = 10,
+    augmentation_upto: bool = False,
     augmentation_pipeline=None,
 ):
     X, Y = fetch_lfw_people(
         min_faces=min_faces,
         max_faces=max_faces,
+        hard_limit=hard_limit,
         shuffle=shuffle,
         random_state=random_state,
         verbose=verbose,
@@ -33,9 +38,22 @@ def export_dataset_objects(
         __x.append(x())
     # exit(1)
 
+    __x = np.array(__x)
+
+    # TODO: Normalize
+    __x = __x / 255
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        __x,
+        Y,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=Y,
+    )
+
     if augment:
         xy = []
-        for i, x in tracker(enumerate(__x)):
+        for i, x in tracker(enumerate(x_train)):
             aug_data = augment_data(
                 x,
                 desired_shape=desired_shape,
@@ -46,7 +64,7 @@ def export_dataset_objects(
             for data in aug_data:
                 data = Image.fromarray(data).convert("L")
                 data = np.array(data)
-                xy.append([data, Y[i]])
+                xy.append([data, y_train[i]])
             # print(xy)
             # fig, axes = plt.subplots(2, 5, figsize=(20, 10))
             # axes = axes.flatten()
@@ -58,15 +76,27 @@ def export_dataset_objects(
             # plt.show()
 
             # exit(1)
-        x_data = np.array(xy)[:, 0]
-        y_data = np.array(xy)[:, 1]
-        x_data.dump("Dataset/x.npy")
-        y_data.dump("Dataset/y.npy")
+        x_train_data = np.array(xy)[:, 0]
+        y_train_data = np.array(xy)[:, 1]
+        x_train_data.dump("Dataset/x_train.npy")
+        y_train_data.dump("Dataset/y_train.npy")
+        x_test.dump("Dataset/x_test.npy")
+        y_test.dump("Dataset/y_test.npy")
+        # og_data = np.array(__x)
+        # og_data.dump("Dataset/x_og.npy")
+        # y_data.dump("Dataset/y.npy")
+
     else:
-        x_data = np.array(__x)
-        x_data.dump("Dataset/x.npy")
-        Y.dump("Dataset/y.npy")
+        # x_data = np.array(__x)
+        # x_data.dump("Dataset/x.npy")
+        # Y.dump("Dataset/y.npy")
+        x_train.dump("Dataset/x_train.npy")
+        y_train.dump("Dataset/y_train.npy")
+        x_test.dump("Dataset/x_test.npy")
+        y_test.dump("Dataset/y_test.npy")
 
 
 if __name__ == "__main__":
-    export_dataset_objects(shuffle=False, min_faces=60, augment=False)
+    export_dataset_objects(
+        shuffle=False, min_faces=60, max_faces=100, hard_limit=False, augment=False
+    )
--- Code/test.py
+++ Code/test.py
@@ -6,6 +6,7 @@ import matplotlib.image as mpimg
 import matplotlib as mpl
 from PIL import Image
 import numpy as np
+from rich import print
 
 if __name__ == "__main__":
     plt.style.use("ggplot")
@@ -68,7 +69,17 @@ if __name__ == "__main__":
     # plt.xscale("log")
     # plt.yscale("log")
     # plt.show()
-    X = np.load("Dataset/x.npy")
-    Y = np.load("Dataset/y.npy")
-    print(X[0].shape)
-    print(Y[0])
+    X = np.load("Dataset/x_train.npy", allow_pickle=True)
+    Y = np.load("Dataset/y_train.npy", allow_pickle=True)
+    x_test = np.load("Dataset/x_test.npy", allow_pickle=True)
+    y_test = np.load("Dataset/y_test.npy", allow_pickle=True)
+    # fig = plt.figure()
+    # ax = fig.add_subplot(xticks=[], yticks=[])
+    # ax.imshow(X[0], cmap="gray")
+    # ax.set_title(Y[0])
+    # plt.show()
+    print(f"{X.shape = }")
+    print(f"{len(set(Y)) = }")
+    print(f"{len(set(y_test)) = }")
+    # print(X[0])
+    # print(Y[0])
